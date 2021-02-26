Add initial More-Thuente step and fix iteration counter off by one

utility/linesearch.py
@@ -7,7 +7,7 @@ from . import parameters
 
 def genericMonotone(lmData, updateCalculator, directionCalculator, lineSearch, f, Df, x):
     """Generic monotone line-search algorithm."""
-    iter = np.array([0, 0])
+    iter = np.array([0, 1])
     fx, gx = f(x), Df(x)
 
     while not stoppingTest(iter, gx):
@@ -23,7 +23,7 @@ def genericMonotone(lmData, updateCalculator, directionCalculator, lineSearch, f
 
 def genericNonmonotone(lmData, updateCalculator, directionCalculator, lineSearch, f, Df, x):
     """Generic nonmonotone line-search algorithm."""
-    iter = np.array([0, 0])
+    iter = np.array([0, 1])
     fx, gx = f(x), Df(x)
     fxList = np.append(np.zeros(parameters.nonmon-1), fx)
 

utility/regularization.py
@@ -10,21 +10,36 @@ the functions can be controlled through the following parameters:
     direction based on the contents of lmData
 """
 import numpy as np
-from . import parameters
+from . import parameters, morethuente
 
 
 def genericMonotone(lmData, updateCalculator, directionCalculator, f, Df, x):
     """Generic monotone regularization method."""
-    iter = np.array([0, 0])
-    fx, gx, lam = f(x), Df(x), 1.0
+    iter = np.array([0, 1])
+    fx, gx, mu = f(x), Df(x), 1
 
-    while not stoppingTest(iter, lam, gx):
-        d = directionCalculator(lmData, lam, gx)
-        pred = 0.5*lam*np.dot(d, d)-0.5*np.dot(gx, d)
+    if stoppingTest(iter, mu, gx):
+        return [x, iter]
+    
+    fun = lambda x: (f(x), Df(x))
+    d = -gx / np.linalg.norm(gx)
+    xn,fn,gn,t,exls,it = \
+        morethuente.cvsrch(fun,len(x),x,fx,gx,d,1,1e-4,0.9,1e-16,1e-20,1e20,20)
+    
+    iter += [1, it]
+    if exls != 1:  # line search failed
+        return [x, iter]
+    
+    updateCalculator(lmData, t * d, gn - gx)
+    x, fx, gx = xn, fn, gn
+
+    while not stoppingTest(iter, mu, gx):
+        d = directionCalculator(lmData, mu, gx)
+        pred = 0.5*mu*np.dot(d, d)-0.5*np.dot(gx, d)
 
         # Check whether predicted reduction is sufficient
         if not pred >= 1e-4*np.linalg.norm(gx)*np.linalg.norm(d):
-            lam *= 4
+            mu *= 4
             continue
 
         # Compute trial point and actual reduction
@@ -32,13 +47,13 @@ def genericMonotone(lmData, updateCalculator, directionCalculator, f, Df, x):
 
         # Check whether iteration was successful
         if (ared <= 1e-4*pred):
-            lam *= 4
+            mu *= 4
             iter += [0, 1]
         else:
             x, fx, gx, yn = acceptTrialPoint(xtry, ftry, Df, gx)
             updateCalculator(lmData, d, yn)
             if (ared >= 0.9*pred):
-                lam = max(1e-4, 0.5*lam)
+                mu = max(1e-4, 0.5*mu)
             iter += [1, 1]
 
     return [x, iter]
@@ -46,17 +61,33 @@ def genericMonotone(lmData, updateCalculator, directionCalculator, f, Df, x):
 
 def genericNonmonotone(lmData, updateCalculator, directionCalculator, f, Df, x):
     """Generic nonmonotone regularization method."""
-    iter = np.array([0, 0])
-    fx, gx, lam = f(x), Df(x), 1.0
+    iter = np.array([0, 1])
+    fx, gx, mu = f(x), Df(x), 1
     fxList = np.append(np.zeros(parameters.nonmon-1), fx)
 
-    while not stoppingTest(iter, lam, gx):
-        d = directionCalculator(lmData, lam, gx)
-        pred = 0.5*lam*np.dot(d, d)-0.5*np.dot(gx, d)
+    if stoppingTest(iter, mu, gx):
+        return [x, iter]
+    
+    fun = lambda x: (f(x), Df(x))
+    d = -gx / np.linalg.norm(gx)
+    xn,fn,gn,t,exls,it = \
+        morethuente.cvsrch(fun,len(x),x,fx,gx,d,1,1e-4,0.9,1e-16,1e-20,1e20,20)
+    
+    iter += [1, it]
+    if exls != 1:  # line search failed
+        return [x, iter]
+    
+    updateCalculator(lmData, t * d, gn - gx)
+    x, fx, gx = xn, fn, gn
+    fxList = np.append(fxList[1:], fx)
+
+    while not stoppingTest(iter, mu, gx):
+        d = directionCalculator(lmData, mu, gx)
+        pred = 0.5*mu*np.dot(d, d)-0.5*np.dot(gx, d)
 
         # Check whether predicted reduction is sufficient
         if not pred >= 1e-4*np.linalg.norm(gx)*np.linalg.norm(d):
-            lam *= 4
+            mu *= 4
             continue
 
         # Compute trial point and actual reduction
@@ -65,25 +96,26 @@ def genericNonmonotone(lmData, updateCalculator, directionCalculator, f, Df, x):
 
         # Check whether iteration was successful
         if (ared <= 1e-4*pred):
-            lam *= 4
+            mu *= 4
             iter += [0, 1]
         else:
             x, fx, gx, yn = acceptTrialPoint(xtry, ftry, Df, gx)
             fxList = np.append(fxList[1:], fx)
             updateCalculator(lmData, d, yn)
             if (ared >= 0.9*pred):
-                lam = max(1e-4, 0.5*lam)
+                mu = max(1e-4, 0.5*mu)
             iter += [1, 1]
 
     return [x, iter]
 
 
-def stoppingTest(iter, lam, gx):
+def stoppingTest(iter, mu, gx):
     """Generic stopping test used for all regularization algorithms."""
     return iter[0] >= parameters.maxIter \
         or iter[1] >= parameters.maxEval \
-        or lam > parameters.maxReg \
-        or np.linalg.norm(gx, np.inf) < parameters.tolGrad
+        or mu > parameters.maxReg \
+        or np.linalg.norm(gx, np.inf) <= parameters.tolGrad
+        #or np.linalg.norm(gx) <= parameters.tolGrad * max(1, np.linalg.norm(x))
 
 
 def computeTrialPoint(x, f, fx, d):